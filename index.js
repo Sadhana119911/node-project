012b2bfb782cb322e411ffd0bb099a7ec2f8c132
Sadhana119911 <sadhanahiremath11@gmail.com>
2026-03-23 15:40:37 +0530

    Initial commit - Node project

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const createServer = (res) => {
+    res.end("Hello, welcome to DevOps");
+};
+
+const h = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    createServer(res);
+});
+
+h.listen(3000, () => {
+    console.log("✅ Server is running at http://localhost:3000");
+});
